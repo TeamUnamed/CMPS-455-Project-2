7db6314708ab85c2b9cb4f11b4fdfa0517e2f8b6
malfact <themalfact@gmail.com>
2023-03-06 19:25:43 -0600

    Implemented argument checking.
    Signed-off-by: malfact <themalfact@gmail.com>

--- src/net/cmps455/unamed/project2/Main.java
+++ src/net/cmps455/unamed/project2/Main.java
@@ -3,7 +3,55 @@ package net.cmps455.unamed.project2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST");
+
+        /* Argument Checking */
+        System.out.println();
+
+        if (args.length == 0) {
+            System.out.println("ERROR: No Arguments Specified");
+            return;
+        }
+
+        if (!args[0].equals("-S")) {
+            System.out.println("ERROR: Invalid Argument '" + args[0] + "'; Only '-S' is permitted.");
+            return;
+        }
+
+        if (args.length == 1) {
+            System.out.println("Error: Missing Argument Value for '-S <VALUE>'");
+            return;
+        }
+
+        int simulationIndex;
+
+        try {
+            simulationIndex = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Argument Value for '-S <VALUE>' is not a number.");
+            return;
+        }
+        /* -------- */
+
+        /* Simulation Index Checking */
+        switch (simulationIndex) {
+            case 1 -> System.out.println("TEMP -> STARTING SIMULATION 1");
+            case 2 -> System.out.println("TEMP -> STARTING SIMULATION 2");
+            case 3 -> System.out.println("TEMP -> STARTING SIMULATION 3");
+            default -> {
+                System.out.println("ERROR: Invalid Simulation (SIMULATION " + simulationIndex + ")");
+                return;
+            }
+        }
+        /* -------- */
+
+        System.out.println("Simulation " + simulationIndex + " selected!");
+        System.out.println();
+
+        // TODO: Start Simulation
+
+        System.out.println();
+        System.out.println("Simulation has ended!");
+        System.out.println("Goodbye!");
     }
 
 }
