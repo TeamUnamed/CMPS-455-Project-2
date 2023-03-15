3f60de68af4fc8c1b2b592bd2e3b326c7c5446fa
malfact <themalfact@gmail.com>
2023-03-15 17:43:23 -0500

    Reverted some changes. I'm dumb.

--- src/net/cmps455/unamed/project2/simulators/Simulator.java
+++ src/net/cmps455/unamed/project2/simulators/Simulator.java
@@ -4,10 +4,10 @@ package net.cmps455.unamed.project2.simulators;
  *  <p>Parent class for all Simulators.
  *  <p>Use {@link Simulator#run()} to start the simulation.
  */
-public abstract class Simulator extends Thread {
+public abstract class Simulator {
 
     /**
      * Start the simulation.
      */
-    public abstract void startSimulation();
+    public abstract void start();
 }
