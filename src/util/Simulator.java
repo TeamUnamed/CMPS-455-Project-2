e2d77b83a84f496d53cc525a431763d8ff0d6521
malfact <themalfact@gmail.com>
2023-03-15 17:03:27 -0500

    Added abstract Simulator.java for each Task's Simulator to inherit from.

---
+++ src/util/Simulator.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+ *  <p>Parent class for all Simulators.
+ *  <p>Use {@link Simulator#run()} to start the simulation.
+ */
+public abstract class Simulator {
+
+    /**
+     * Start the simulation.
+     */
+    public abstract void run();
+}
