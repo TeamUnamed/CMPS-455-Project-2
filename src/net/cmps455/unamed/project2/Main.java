e015c45071ae44d4f47cb8e91220111c540ed7a8
malfact <themalfact@gmail.com>
2023-03-15 16:46:45 -0500

    Added check for too many arguments. Waiting for #17, #18, #19 to test full functionality.

--- src/net/cmps455/unamed/project2/Main.java
+++ src/net/cmps455/unamed/project2/Main.java
@@ -7,26 +7,30 @@ public class Main {
         /* Argument Checking */
         System.out.println();
 
-        if (args.length == 0) {
+        if (args.length == 0) { // return on no arguments
             System.out.println("ERROR: No Arguments Specified");
             return;
         }
 
-        if (!args[0].equals("-S")) {
+        if (!args[0].equals("-S")) { // return on argument no '-S'
             System.out.println("ERROR: Invalid Argument '" + args[0] + "'; Only '-S' is permitted.");
             return;
         }
 
-        if (args.length == 1) {
+        if (args.length == 1) { // return on no argument value
             System.out.println("Error: Missing Argument Value for '-S <VALUE>'");
             return;
+        }
+
+        if (args.length > 2) { // return on too many arguments
+            System.out.println("Error: Too Many Arguments detected!");
         }
 
         int simulationIndex;
 
         try {
             simulationIndex = Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e) { // return on argument value not ia number
             System.out.println("Error: Argument Value for '-S <VALUE>' is not a number.");
             return;
         }
@@ -37,7 +41,7 @@ public class Main {
             case 1 -> System.out.println("TEMP -> STARTING SIMULATION 1");
             case 2 -> System.out.println("TEMP -> STARTING SIMULATION 2");
             case 3 -> System.out.println("TEMP -> STARTING SIMULATION 3");
-            default -> {
+            default -> { // return on invalid simulation
                 System.out.println("ERROR: Invalid Simulation (SIMULATION " + simulationIndex + ")");
                 return;
             }
