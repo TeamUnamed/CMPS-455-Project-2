6fc396f6e8668e5fe020e60cf97afb01f9b62c9b
malfact <themalfact@gmail.com>
2023-03-15 17:26:15 -0500

    Made Simulator extend Thread and renamed Simulator.run to Simulator.start.

--- src/net/cmps455/unamed/project2/util/Simulator.java
+++ src/net/cmps455/unamed/project2/util/Simulator.java
@@ -4,10 +4,10 @@ package net.cmps455.unamed.project2.util;
  *  <p>Parent class for all Simulators.
  *  <p>Use {@link Simulator#run()} to start the simulation.
  */
-public abstract class Simulator {
+public abstract class Simulator extends Thread {
 
     /**
      * Start the simulation.
      */
-    public abstract void run();
+    public abstract void start();
 }
