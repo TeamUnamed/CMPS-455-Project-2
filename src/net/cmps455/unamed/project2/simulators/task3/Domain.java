d3a44a0fb79ebee1e8cf643b9d4a00a725288e26
malfact <themalfact@gmail.com>
2023-03-16 18:43:39 -0500

    Removed Domain.getName(), unused and conflicts with Thread.

--- src/net/cmps455/unamed/project2/simulators/task3/Domain.java
+++ src/net/cmps455/unamed/project2/simulators/task3/Domain.java
@@ -2,16 +2,12 @@ package net.cmps455.unamed.project2.simulators.task3;
 
 import net.cmps455.unamed.project2.AccessLocked;
 
-public class Domain implements AccessLocked {
+public class Domain extends Thread implements AccessLocked {
 
     private String name;
 
     public Domain(String name) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 
     @Override
