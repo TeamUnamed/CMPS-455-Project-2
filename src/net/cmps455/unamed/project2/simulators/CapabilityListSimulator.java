5bbc1f8e35847499ccd553ff4f5d1a9dd51aecbd
malfact <themalfact@gmail.com>
2023-03-19 00:51:04 -0500

    Updated Simulator to reflect System changes.

--- src/net/cmps455/unamed/project2/simulators/CapabilityListSimulator.java
+++ src/net/cmps455/unamed/project2/simulators/CapabilityListSimulator.java
@@ -22,86 +22,54 @@ public class CapabilityListSimulator extends Simulator {
         System.out.printf("| %d Objects\n", objectCount);
         System.out.println();
 
-        /* Setup */
-
         // Create Array of LinkedLists
         // CapabilityList inherits from LinkedList
         CapabilityList[] capabilityLists = new CapabilityList[domainCount];
 
-        // Create array to store Domains & Files
-        VirtualObject[] objects = new VirtualObject[domainCount + objectCount];
 
-        // Create Arbitrator and give a pointer to Capabilities
-        Arbitrator arbitrator = new Arbitrator(capabilityLists);
+        VirtualSystem system = VirtualSystem.NewInternal();
 
-
-        // Generate Objects
-        for (int i = 0; i < objectCount; i++) {
-            objects[i] = new DummyObject(i+1);
-        }
+        Domain[] domains = new Domain[domainCount];
+        File[] files = new File[objectCount];
 
         // Generate Domains
         for (int i = 0; i < domainCount; i++) {
-            objects[i + objectCount] = new Domain(i+1, objects, arbitrator);
+            domains[i] = system.newDomain("D" + (i+1));
         }
 
-        // Generate and fill Capability Lists
+        // Generate Objects
+        for (int i = 0; i < objectCount; i++) {
+            File file = system.newFile("F" + (i+1));
+            files[i] = file;
+        }
+
+        // Generate Capabilities
         for (int i = 0; i < domainCount; i++) {
-            capabilityLists[i] = new CapabilityList((VirtualDomain) objects[i + objectCount]);
-
-            for (int j = 0; j < domainCount + objectCount; j++) {
-                int flag = random.nextInt(4); // [0,3]
-
-                VirtualObject object = objects[j];
-                if (object.isDomain())
-                    flag = (flag / 2) * 4; // [0,3] -> [0,1] -> 0 or 4
-
-                capabilityLists[i].add(Capability.create(objects[i],flag));
+            // For Files
+            for (int j = 0; j < objectCount; j++) {
+                system.setPermission(domains[i], files[j], random.nextInt(4));
             }
-
-            System.out.println(capabilityLists[i]);
+            // For other Domains
+            for (int j = 0; j < domainCount; j++) {
+                system.setPermission(domains[i], domains[j], random.nextInt(2) * 3);
+            }
+            // Print out CapabilityLists
+            System.out.println(system.getPermissions(domains[i]));
         }
 
-        /* Simulation */
-
+        // Instantiate Domain Threads
+        DomainThread[] domainThreads = new DomainThread[domainCount];
         for (int i = 0; i < domainCount; i++) {
-            ((Domain) objects[i + objectCount]).start();
+            domainThreads[i] = new DomainThread(domains[i], system.For(domains[i]));
+            domainThreads[i].start();
         }
 
         for (int i = 0; i < domainCount; i++) {
             try {
-                ((Domain) objects[i + objectCount]).join();
+                domainThreads[i].join();
             } catch (InterruptedException e) {
                 System.out.println("Error: " + e);
             }
         }
     }
-
-    private static class DummyObject implements VirtualObject {
-
-        public final int id;
-
-        DummyObject (int id) {
-            this.id = id;
-        }
-
-        public void read() {
-
-        }
-
-        public void write() {
-
-        }
-
-        @Override
-        public String toString() {
-            return "F" + id;
-        }
-
-        @Override
-        public int getID() {
-            return id;
-        }
-    }
-
 }
