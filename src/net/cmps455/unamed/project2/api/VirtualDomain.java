693f69018fc46fd2d0501d9832d76549490e3099
malfact <themalfact@gmail.com>
2023-03-21 14:48:34 -0500

    Added constant actions to file and domain to represent possible capabilities.

--- src/net/cmps455/unamed/project2/api/VirtualDomain.java
+++ src/net/cmps455/unamed/project2/api/VirtualDomain.java
@@ -1,11 +1,17 @@
 package net.cmps455.unamed.project2.api;
 
+import net.cmps455.unamed.project2.api.action.VirtualAction;
 import net.cmps455.unamed.project2.api.system.VirtualSystem;
 
 /**
  * Reference Container to a domain in {@link VirtualSystem}.
  */
 public class VirtualDomain extends VirtualObject {
+
+    /**
+     * Action representing the ability to switch to this domain.
+     */
+    public static final VirtualAction SWITCH = new VirtualAction("switch");
 
     public VirtualDomain(VirtualSystem.SystemID id, String name) {
         super(id, name);
--- src/net/cmps455/unamed/project2/api/file/VirtualFile.java
+++ src/net/cmps455/unamed/project2/api/file/VirtualFile.java
@@ -1,12 +1,26 @@
 package net.cmps455.unamed.project2.api.file;
 
 import net.cmps455.unamed.project2.api.VirtualObject;
+import net.cmps455.unamed.project2.api.action.VirtualAction;
 import net.cmps455.unamed.project2.api.system.VirtualSystem;
 
 /**
  * Reference Container to a file in {@link VirtualSystem}.
  */
 public class VirtualFile extends VirtualObject {
+
+    /**
+     * Action representing ability to read from a file.
+     */
+    public static final VirtualAction READ = new VirtualAction("read");
+    /**
+     * Action representing ability to write to a file.
+     */
+    public static final VirtualAction WRITE = new VirtualAction("write");
+    /**
+     * Action representing ability to read from or write to a file.
+     */
+    public static final VirtualAction READ_WRITE = new VirtualAction("read/write", READ, WRITE);
 
     public VirtualFile(VirtualSystem.SystemID id, String name) {
         super(id, name);
